3fa8649851b7788dff71ebb8123864f8c872c2e8
Malek <106095300+MalekD5@users.noreply.github.com>
2024-11-05 20:45:13 +0300

    Initiate project files

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+int main(void)
+{
+    WSADATA wsaData;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        fprintf(stderr, "WSAStartup failed.\n");
+        exit(1);
+    }
+
+    if (LOBYTE(wsaData.wVersion) != 2 ||
+        HIBYTE(wsaData.wVersion) != 2)
+    {
+        fprintf(stderr,"Versiion 2.2 of Winsock is not available.\n");
+        WSACleanup();
+        exit(2);
+    }
+
+    printf("Socket is ready");
+    return 0;
+}
